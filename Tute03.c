cd2dab23ddd175501282fb5afc35145cc2067d01
IT21201882 <99796488+IT21201882@users.noreply.github.com>
2022-02-16 12:40:46 +0000

    IT21201882 complete

--- Tute03.c
+++ Tute03.c
@@ -10,9 +10,19 @@ sum = 1+2+3+....+ 99+100 = 5050
 
 n -> 1-
 sum = 1+2+3+...+10 = 55 */
-
+    
 #include <stdio.h>
 int main() {
+  int num, i, sum;
+  printf("Enter the value from keybord : ");
+  scanf("%d",&num);
+  for(i=1; i<=num; i++)
+  {
+     sum +=i;   
+  }
+
+  printf("Sum : %d",sum);
+  
   
   return 0;
 }
